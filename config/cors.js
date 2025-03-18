69f7daec9ee76e74611272d258c27ac9032446c8
akhil soigama <akhilsoigama@gmail.com>
2025-03-18 13:47:35 +0530

    add

--- config/cors.js
+++ config/cors.js
@@ -3,21 +3,23 @@ const cors = require('cors');
 const isProduction = process.env.NODE_ENV === 'production';
 
 const allowedOrigins = isProduction
-  ? ['https://boosters-sooty.vercel.app']
-  : ['http://localhost:3000', 'http://localhost:5000','https://booster-server-one.vercel.app'];
+  ? ['https://boosters-sooty.vercel.app', 'https://booster-server-one.vercel.app']
+  : ['http://localhost:3000', 'http://localhost:5000'];
 
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
+
     if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
+      console.log(`Blocked by CORS: ${origin}`);
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true,  
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['set-cookie'],
 };
 
